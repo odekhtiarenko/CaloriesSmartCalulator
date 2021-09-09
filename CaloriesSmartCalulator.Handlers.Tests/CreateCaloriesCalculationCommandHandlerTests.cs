6f00d63b491a7a7d4e73520a3a73435cd7438764
odekhtiarenko <oleg.dekhtiarenko@gmail.com>
2021-09-09 15:47:21 +0300

    Add create task handler

---
+++ CaloriesSmartCalulator.Handlers.Tests/CreateCaloriesCalculationCommandHandlerTests.cs
@@ -0,0 +1,68 @@
+﻿using AutoFixture;
+using CaloriesSmartCalulator.DAL;
+using CaloriesSmartCalulator.Data.Entities;
+using CaloriesSmartCalulator.Handlers.CommandHandlers;
+using CaloriesSmartCalulator.Handlers.Contracts.Commands;
+using FluentAssertions;
+using Moq;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CaloriesSmartCalulator.Handlers.Tests
+{
+    public class CreateCaloriesCalculationCommandHandlerTests
+    {
+        private readonly CreateCaloriesCalculationCommandHandler _handler;
+        private readonly Mock<IRepository<CaloriesCalculationTask>> _repositoryMoq;
+        private readonly IFixture _fixture;
+
+        public CreateCaloriesCalculationCommandHandlerTests()
+        {
+            _fixture = new Fixture();
+            _fixture.Behaviors.OfType<ThrowingRecursionBehavior>().ToList()
+                                .ForEach(b => _fixture.Behaviors.Remove(b));
+
+            _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+
+            _repositoryMoq = new Mock<IRepository<CaloriesCalculationTask>>();
+            _handler = new CreateCaloriesCalculationCommandHandler(_repositoryMoq.Object);
+        }
+
+        [Fact]
+        public async Task Handle_ShouldReuturnSuccessfulResult()
+        {
+            var task = _fixture.Create<CaloriesCalculationTask>();
+            _repositoryMoq.Setup(x => x.InsertAsync(It.IsAny<CaloriesCalculationTask>())).ReturnsAsync(task);
+
+            var result = await _handler.Handle(_fixture.Create<CreateCaloriesCalculationCommand>(), new CancellationToken());
+
+            result.Success
+                .Should()
+                .BeTrue();
+
+            result.Value
+                .Should()
+                .Be(task.Id);
+        }
+
+        [Fact]
+        public async Task Handle_ShouldReuturnFailedResultOnException()
+        {
+            var task = _fixture.Create<CaloriesCalculationTask>();
+            var exceptionMessage = "Exception message";
+            _repositoryMoq.Setup(x => x.InsertAsync(It.IsAny<CaloriesCalculationTask>())).ThrowsAsync(new System.Exception(exceptionMessage));
+
+            var result = await _handler.Handle(_fixture.Create<CreateCaloriesCalculationCommand>(), new CancellationToken());
+
+            result.Success
+                .Should()
+                .BeFalse();
+
+            result.FailureMessage
+                .Should()
+                .Be(exceptionMessage);
+        }
+    }
+}
---
+++ CaloriesSmartCalulator.Handlers/CommandHandlers/CreateCaloriesCalculationCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using CaloriesSmartCalulator.DAL;
+using CaloriesSmartCalulator.Data.Entities;
+using CaloriesSmartCalulator.Handlers.Contracts.Commands;
+using CaloriesSmartCalulator.Handlers.Contracts.Results;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CaloriesSmartCalulator.Handlers.CommandHandlers
+{
+    public class CreateCaloriesCalculationCommandHandler : IRequestHandler<CreateCaloriesCalculationCommand, CreateCaloriesCalculationResult>
+    {
+        private readonly IRepository<CaloriesCalculationTask> _caloriesCalculationTaskRespository;
+
+        public CreateCaloriesCalculationCommandHandler(IRepository<CaloriesCalculationTask> caloriesCalculationTaskRespository)
+        {
+            _caloriesCalculationTaskRespository = caloriesCalculationTaskRespository;
+        }
+
+        public async Task<CreateCaloriesCalculationResult> Handle(CreateCaloriesCalculationCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var task = new CaloriesCalculationTask();
+                task.CaloriesCalculationTaskItems = request.Products.Select(v => new CaloriesCalculationTaskItem() { Product = v }).ToList();
+                var result = await _caloriesCalculationTaskRespository.InsertAsync(task);
+
+                return new CreateCaloriesCalculationResult(result.Id);
+            }
+            catch (Exception ex)
+            {
+                return new CreateCaloriesCalculationResult(ex);
+            }
+        }
+    }
+}
