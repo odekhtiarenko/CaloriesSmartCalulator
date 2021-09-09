5e33dbb8167895375da64efa65928cdf95a2b45b
odekhtiarenko <oleg.dekhtiarenko@gmail.com>
2021-09-09 22:21:37 +0300

    add production config secret manager

---
+++ CaloriesSmartCalulator/Extentions/ConfigurationBuilderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Azure.Extensions.AspNetCore.Configuration.Secrets;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace CaloriesSmartCalulator.Extentions
+{
+    public static class ConfigurationBuilderExtensions
+    {
+        public static void ConfigureKeyVault(this IConfigurationBuilder config)
+        {
+            var keyVaultEndpoint = Environment.GetEnvironmentVariable("SECRET_MANAGER_URL");
+
+            if (keyVaultEndpoint is null)
+                throw new InvalidOperationException("Store the Key Vault endpoint in a SECRET_MANAGER_URL environment variable.");
+
+            var secretClient = new SecretClient(new(keyVaultEndpoint), new DefaultAzureCredential());
+            config.AddAzureKeyVault(secretClient, new KeyVaultSecretManager());
+        }
+
+    }
+}
--- CaloriesSmartCalulator/Program.cs
+++ CaloriesSmartCalulator/Program.cs
@@ -1,3 +1,4 @@
+using CaloriesSmartCalulator.Extentions;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -20,6 +21,19 @@ namespace CaloriesSmartCalulator
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.ConfigureAppConfiguration((hostingContext, config) =>
+                    {
+                        var env = hostingContext.HostingEnvironment;
+
+                       
+                        if (env.IsProduction())
+                            config.ConfigureKeyVault();
+                        else
+                            config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                             .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                    });
+
+
                     webBuilder.UseStartup<Startup>();
                 });
     }
